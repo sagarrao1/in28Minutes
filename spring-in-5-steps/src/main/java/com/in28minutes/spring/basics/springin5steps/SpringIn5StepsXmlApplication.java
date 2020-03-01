df06b7ea60c40350b536c6c90497a09241dfa7d3
sagarrao1 <sagarrao1@gmail.com>
2020-03-01 18:50:37 +0530

    till setp 26

--- spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsXmlApplication.java
+++ spring-in-5-steps/src/main/java/com/in28minutes/spring/basics/springin5steps/SpringIn5StepsXmlApplication.java
@@ -4,6 +4,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import com.in28minutes.spring.basics.springin5steps.properties.ExternalXmlService;
 import com.in28minutes.spring.basics.springin5steps.xml.XmlPersonDAO;
 	   
 public class SpringIn5StepsXmlApplication {
@@ -25,6 +26,10 @@ public class SpringIn5StepsXmlApplication {
 		XmlPersonDAO xmlPersonDAO = applicationContext.getBean(XmlPersonDAO.class);		
 		LOGGER.info("info {}", xmlPersonDAO );
 		LOGGER.info("info 2{}", xmlPersonDAO.getXmlJdbcConnection());
+		
+		ExternalXmlService externalXmlService = applicationContext.getBean(ExternalXmlService.class);	
+		LOGGER.info("info 3 {}", externalXmlService );
+		LOGGER.info("info 4 {}", externalXmlService.getUrl() );
 		}
 	}
 

591245a910082846c7c262dcdc68d95d864df529
sagarrao1 <sagarrao1@gmail.com>
2020-03-02 20:47:55 +0530

    spring boot step 2

--- springboot-in-10-steps/src/main/java/com/in28Minutes/springboot/basics/springbootin10steps/SpringbootIn10StepsApplication.java
+++ springboot-in-10-steps/src/main/java/com/in28Minutes/springboot/basics/springbootin10steps/SpringbootIn10StepsApplication.java
@@ -2,12 +2,20 @@ package com.in28Minutes.springboot.basics.springbootin10steps;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootApplication
 public class SpringbootIn10StepsApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootIn10StepsApplication.class, args);
+		ApplicationContext applicationContext = SpringApplication.run(SpringbootIn10StepsApplication.class, args);
+		
+		String[] beanDefinitionNames = applicationContext.getBeanDefinitionNames();
+		
+		for (String string : beanDefinitionNames) {
+			System.out.println(string);
+		}
+			
 	}
 
 }

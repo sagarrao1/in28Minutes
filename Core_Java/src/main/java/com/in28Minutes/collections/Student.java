22a70603a9fe11cc6cdb7531c92207c8b1400103
sagarrao1 <sagarrao1@gmail.com>
2020-04-05 07:02:59 +0530

    set and queue collections

--- Core_Java/src/main/java/com/in28Minutes/collections/Student.java
+++ Core_Java/src/main/java/com/in28Minutes/collections/Student.java
@@ -4,10 +4,25 @@ public class Student implements Comparable<Student> {
 	private int id;
 	private String name;
 
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
 
 	public Student(int id, String name) {
 		super();
 		this.id = id;
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
 		this.name = name;
 	}
 
@@ -21,6 +36,8 @@ public class Student implements Comparable<Student> {
 //		return ((this.id < that.id) ? -1 : (this.id == that.id) ? 0 : 1); // old way
 		return Integer.compare(this.id, that.id); // new way from 1.7
 		// for reverse you just need to reverse (that.is,this.id)
+
+//		return (this.id - that.id); // another simple way
 	}
 
 

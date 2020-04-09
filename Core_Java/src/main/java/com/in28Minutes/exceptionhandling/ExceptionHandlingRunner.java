49d10f171f6a698065e1be1b180edaea2aa76dd2
sagarrao1 <sagarrao1@gmail.com>
2020-04-09 22:11:40 +0530

    exceptionhandling

---
+++ Core_Java/src/main/java/com/in28Minutes/exceptionhandling/ExceptionHandlingRunner.java
@@ -0,0 +1,18 @@
+package com.in28Minutes.exceptionhandling;
+
+public class ExceptionHandlingRunner {
+
+	public static void main(String[] args) {
+		method1();
+	}
+
+	private static void method1() {
+		method2();
+	}
+
+	private static void method2() {
+		String str= null;
+		str.length();
+	}
+	
+}

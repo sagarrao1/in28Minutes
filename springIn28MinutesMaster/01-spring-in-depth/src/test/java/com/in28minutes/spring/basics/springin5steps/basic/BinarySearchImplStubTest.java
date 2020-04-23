4d98ca8b77b44b4d744f2ab283e04f5ca3d1cad8
sagarrao1 <sagarrao1@gmail.com>
2020-04-23 07:47:22 +0530

    spring test

--- springIn28MinutesMaster/01-spring-in-depth/src/test/java/com/in28minutes/spring/basics/springin5steps/basic/BinarySearchImplStubTest.java
+++ springIn28MinutesMaster/01-spring-in-depth/src/test/java/com/in28minutes/spring/basics/springin5steps/basic/BinarySearchImplStubTest.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
-public class BinarySearchImplTest {
+public class BinarySearchImplStubTest {
 
 	@Test
 	public void binarySearchGivenNum() {
--- springIn28MinutesMaster/01-spring-in-depth/src/test/java/com/in28minutes/spring/basics/springin5steps/basic/BinarySearchImplTest_Step5BasicApplicationTest.java
+++ springIn28MinutesMaster/01-spring-in-depth/src/test/java/com/in28minutes/spring/basics/springin5steps/basic/BinarySearchImplTest_Step5BasicApplicationTest.java
@@ -10,20 +10,21 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 import com.in28minutes.spring.basics.springin5steps.SpringIn5StepsBasicApplication;
 
+//Load the context
 @RunWith(SpringRunner.class)
-@ContextConfiguration(classes=SpringIn5StepsBasicApplication.class)
+@ContextConfiguration(classes = SpringIn5StepsBasicApplication.class)
 public class BinarySearchImplTest_Step5BasicApplicationTest {
+	
+//     Get bean from context
 	@Autowired
 	BinarySearchImpl binarySearchImpl;
 	
 	@Test
-	public void binarySearchGivenNum() {
-		
-		int result = binarySearchImpl.binarySearch(new int[] {12,4,8}, 4);
+	public void testbinarySearchGivenNum() {
+//		check method on binarySearchImpl
+//		check if the value is correct or not
+		int result = binarySearchImpl.binarySearch(new int[] {2,12,3},3);
 		assertEquals(3, result);
-		
 	}
 
 }
-
-

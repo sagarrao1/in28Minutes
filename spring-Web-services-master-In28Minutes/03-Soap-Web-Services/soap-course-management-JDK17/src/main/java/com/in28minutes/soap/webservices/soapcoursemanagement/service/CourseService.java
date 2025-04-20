7c920f407d836750e408f6ed685396e092510146
sagarrao1 <sagarrao1@gmail.com>
2025-04-20 19:15:55 +0530

    updated to jdk17

---
+++ spring-Web-services-master-In28Minutes/03-Soap-Web-Services/soap-course-management-JDK17/src/main/java/com/in28minutes/soap/webservices/soapcoursemanagement/service/CourseService.java
@@ -0,0 +1,64 @@
+package com.in28minutes.soap.webservices.soapcoursemanagement.service;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+
+import com.in28minutes.soap.webservices.soapcoursemanagement.bean.Course;
+
+@Component
+public class CourseService {
+	
+	// findByID  input : id output : Course
+	// GetallCourses  input: nothing , output : List<Course>
+	// deleteByCourseID , input: id, output: int
+	
+
+	public enum Status{
+		SUCCESS,FAILURE;
+	}
+
+	public Course findById(int id) {
+		for (Course course : courseList) {
+			if (course.getId() == id) {
+				return course;
+			}
+		}		
+		return null;
+	}
+	
+	public List<Course> findAll(){		
+		return courseList;
+	}
+	
+	
+	public Status deleteCourse(int id){		
+//		-- deleting in for LinkLoop may get Exception so we use Iterator		
+		Iterator<Course> itr = courseList.iterator();		
+		while(itr.hasNext()) {
+			Course course = itr.next();
+			if(course.getId()==id) {
+				itr.remove();
+				return Status.SUCCESS;
+			}
+		}
+		return Status.FAILURE;
+	}
+	
+
+	private static List<Course> courseList =  new ArrayList<>();	
+	
+	  static {		
+		Course course1= new Course(1,"Python","learn python in 30 days");		
+		courseList.add(course1);		
+		Course course2= new Course(2,"Mongo DB","learn mongo DB New");
+		courseList.add(course2);
+		Course course3= new Course(3,"Java REST service","learn REST which is useful for  in 30 days");
+		courseList.add(course3);
+		Course course4 = new Course(4, "Maven", "Most popular maven course on internet!");
+		courseList.add(course4);
+	}
+	
+}
